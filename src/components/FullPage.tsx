ac80b006b573c10fa2084a0bb8837ae3f88f0383
Nick Beukema <beukeman@fwf.com>
2025-09-13 12:41:01 -0400

    lower size

--- src/components/FullPage.tsx
+++ src/components/FullPage.tsx
@@ -8,14 +8,14 @@ export default function FullPage() {
         <Container maxWidth="xl" sx={{ height: '100vh', py: 2 }}>
             <Grid container spacing={4} sx={{ height: '100%' }}>
                 {/* Calendar section (top) */}
-                <Grid size={12} sx={{ height: '70%' }}>
+                <Grid size={12} sx={{ height: '80%' }}>
                     <Paper elevation={3} sx={{ p: 2, height: '100%' }}>
                         <Calendar />
                     </Paper>
                 </Grid>
 
                 {/* Task list section (bottom) */}
-                <Grid size={12} sx={{ height: '27%' }}>
+                <Grid size={12} sx={{ height: '17%' }}>
                     <Paper elevation={3} sx={{ p: 2, height: '100%' }}>
                         <Typography variant="h6" gutterBottom>
                             Task List
